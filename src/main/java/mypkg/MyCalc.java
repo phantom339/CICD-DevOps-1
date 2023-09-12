abadbed87974b16e086590b566c6decc2f96fd32
unknown <Administrator@HITESHKUMAR_LTP.DDN.UPES.AC.IN>
2023-09-12 09:48:00 +0530

    mul added

--- src/main/java/mypkg/MyCalc.java
+++ src/main/java/mypkg/MyCalc.java
@@ -11,6 +11,10 @@ public class MyCalc {
 	{
 		return(a-b);		
 	}
+	public int mul(int a, int b)
+	{
+		return(a*b);		
+	}
 	public static void main(String[] args) {
 		int num1, num2;
 		MyCalc calc = new MyCalc();
